3fd288725a6af36e408e9176a6d0fe12b6c0140d
Omar Abdelmoneim Gamal <58474037+OmarAGamal@users.noreply.github.com>
2023-10-08 11:00:43 +0300

    rfsensor.py

--- rfsensor.py
+++ rfsensor.py
@@ -21,3 +21,33 @@ def setup_platform(
         return
     # add_entities([ExampleSensor()])
     add_entities([EchoHandler()])
+class EchoHandler(asyncore.dispatcher_with_send):
+
+    def handle_read(self):
+        data = self.recv(8192)
+        # if data:
+        #     try:
+        #         send_list = preprocess_packet(data)
+        #         if send_list != 0 :
+        #             for i in send_list :
+        #                 self.send(i)
+        #     except :
+        #         pass
+
+
+class EchoServer(asyncore.dispatcher):
+
+    def __init__(self, host, port):
+        asyncore.dispatcher.__init__(self)
+        self.create_socket()
+        self.set_reuse_addr()
+        self.bind((host, port))
+        self.listen(5)
+
+    def handle_accepted(self, sock, addr):
+        print('Incoming connection from %s' % repr(addr))
+        handler = EchoHandler(sock)
+
+
+server = EchoServer('', 2000)
+asyncore.loop()
