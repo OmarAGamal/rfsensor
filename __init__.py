44b9b7ce0727edf0e7a729794edd1a9c99f715b7
Omar Abdelmoneim Gamal <58474037+OmarAGamal@users.noreply.github.com>
2023-10-08 15:06:40 +0300

    __init__.py

--- __init__.py
+++ __init__.py
@@ -1,14 +1,14 @@
 """RF Sensor integration"""
 
-from __future__ import annotations
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.typing import ConfigType
+# from __future__ import annotations
+# from homeassistant.core import HomeAssistant
+# from homeassistant.helpers.typing import ConfigType
 
-DOMAIN = 'rfsensor'
+# DOMAIN = 'rfsensor'
 
-def setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    """Your controller/hub specific code."""
-    hass.data[DOMAIN] = {}
+# def setup(hass: HomeAssistant, config: ConfigType) -> bool:
+#     """Your controller/hub specific code."""
+#     hass.data[DOMAIN] = {}
   
-    hass.helpers.discovery.load_platform('sensor', DOMAIN, {}, config)
-    return True
+#     hass.helpers.discovery.load_platform('sensor', DOMAIN, {}, config)
+#     return True
