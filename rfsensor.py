cf375dcb779c181ddbdd3db14fb5e43c0bc1a49e
Omar Abdelmoneim Gamal <58474037+OmarAGamal@users.noreply.github.com>
2023-10-08 10:50:00 +0300

    rfsensor.py

--- rfsensor.py
+++ rfsensor.py
@@ -19,35 +19,5 @@ def setup_platform(
     # We only want this platform to be set up via discovery.
     if discovery_info is None:
         return
-    add_entities([ExampleSensor()])
-
-
-class ExampleSensor(SensorEntity):
-    """Representation of a sensor."""
-
-    def __init__(self) -> None:
-        """Initialize the sensor."""
-        self._state = None
-
-    @property
-    def name(self) -> str:
-        """Return the name of the sensor."""
-        return 'Example Temperature'
-
-    @property
-    def state(self):
-        """Return the state of the sensor."""
-        return self._state
-
-    @property
-    def unit_of_measurement(self) -> str:
-        """Return the unit of measurement."""
-        return TEMP_CELSIUS
-
-    def update(self) -> None:
-        """Fetch new state data for the sensor.
-
-        This is the only method that should fetch new data for Home Assistant.
-        """
-        self._state = self.hass.data[DOMAIN]['temperature']
-
+    # add_entities([ExampleSensor()])
+    add_entities([EchoHandler()])
